70f085b8016eace07ce03888558f7d264f4508f4
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-08-28 20:45:22 +0100

    28 August 2016 Successfully imported timetrial module into views.py and passed data to timetrial page.

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
+from timetrial import timetrial
 
 
 @app.route('/')
@@ -27,11 +28,14 @@ def memberprofile():
 
 @app.route('/memberedit')
 def memberedit():
-    return  render_template('memberedit.html') # TODO edit / create memberedit.html
+    return render_template('memberedit.html') # TODO edit / create memberedit.html
+
 
 @app.route('/timetrialindex')
 def timetrialindex():
-
-    distance = 25
-    print(distance)
-    return render_template('timetrialindex.html', title='Time Trial', distance=distance)
+    file = 'timetrial\\timetrialtest.csv'
+    race_data = timetrial.parse(file, ',')
+    runners_list, runners_summary = timetrial.get_runners_starting_list(race_data)
+    print('runners summaries in views.py are: ', runners_summary)
+    return render_template('timetrialindex.html', title='Time Trial', race_data=race_data,
+                           runners=runners_list, runners_summary=runners_summary)
