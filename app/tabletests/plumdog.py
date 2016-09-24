6ec284acb338202d62c8a8f65baf3962dae23443
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-09-24 11:49:57 +0100

    24 September 2016 Installed flask_table to see if it helps with displaying results in tables. Added test in workingfile.py that creates table of results in terminal. Next step : create working template so that I can use html to present to web page

--- app/tabletests/plumdog.py
+++ app/tabletests/plumdog.py
@@ -1,16 +1,10 @@
-﻿# import things
+# import things
 from flask_table import Table, Col
-import timetrial
-import os
+
 # Declare your table
-
-
 class ItemTable(Table):
-    Runner = Col('Runner')
-    Digitime = Col('Digitime')
-    Time = Col('Time')
-    Date = Col('Date')
-    Pace = Col('Pace')
+    name = Col('Name')
+    description = Col('Description')
 
 
 # Get some objects
@@ -19,14 +13,10 @@ class Item(object):
         self.name = name
         self.description = description
 # Or, equivalently, some dicts
-items2 = [dict(name='Name1', description='Description1'),
+items = [dict(name='Name1', description='Description1'),
          dict(name='Name2', description='Description2'),
          dict(name='Name3', description='Description3')]
 
-filename = 'timetrialtest.csv'  # this is a test file
-timetrial_file = os.path.join('', filename)
-race_data = timetrial.parse(timetrial_file, ',')
-items = race_data
 # Populate the table
 table = ItemTable(items)
 
