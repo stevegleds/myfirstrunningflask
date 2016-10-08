ec4c9e5395271ba33015ffa6df5c78ee12623010
Steve Gledhill <stevegleds@users.noreply.github.com>
2016-10-08 20:05:52 +0100

    08 October 2016 Created timetrialpandas to try out pandas for csv trying to sort date types such as float and date. Seems to be OK. Works with floats but dates are strings

---
+++ timetrial/timetrialpandas.py
@@ -0,0 +1,25 @@
+import csv
+import os
+import pandas as pd
+import pandas_datareader
+from pandas import DataFrame
+import datetime
+from pandas_datareader import data, wb
+
+
+TIME_TRIAL_DISTANCE = 3.0
+SLOWEST_PACE = 99
+
+# Working through https://automatetheboringstuff.com/chapter12/#calibre_link-64
+# 10 July 2015 branched to follow newcoder.io tutorial on data visualization
+# 11 November 2015 Added to desktop
+
+# RUN_FILE = 'timetrial/timetrial.csv' # this is  the full file
+filename = 'timetrialtest.csv'  # this is a test file
+timetrial_file = os.path.join('timetrial', filename)
+
+df = pd.read_csv(filename, parse_dates=True)
+#  print(df.head())
+print(df[df.Date > "05-Oct-15"])
+print(df[df.Date < "05-Oct-15"])
+print(df[df.Date == "05-Oct-15"])
